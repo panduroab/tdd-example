51235a825c00cff16a462d39723c56385ec07843
GerardoGuti <developer@MA-E15-1009.local>
2018-01-16 10:42:10 -0600

    "Calculadora Basica "

--- Mathy/index.js
+++ Mathy/index.js
@@ -1,9 +1,6 @@
-module.exports = () => {
-    return {
-        multiply: (number1, number2) => {
-            return new Promise((resolve, reject) => {
-                return resolve(200);
-            });
-        }
-    }
-};
+module.exports = () => ({ 
+    multiply  : (number1, number2) => {return new Promise((resolve, reject) => resolve(number1*number2))},
+    add       : (number1, number2) => {return new Promise((resolve, reject) => resolve(number1+number2))},
+    substract : (number1, number2) => {return new Promise((resolve, reject) => resolve(number1-number2))},
+    divide    : (number1, number2) => {return new Promise((resolve, reject) => resolve(number1/number2))}      
+});
--- __tests__/Mathy/mathy.expect.js
+++ __tests__/Mathy/mathy.expect.js
@@ -2,18 +2,66 @@ const Mathy = require('../../Mathy');
 const assert = require('chai').assert;
 
 describe('Math example', () => {
-    it('should multiply tow numbers', done => {
+    let arg1 =27,arg2=93 ;
+    it('Should multiply two numbers', done => {//Multiplica dos numeros
         //Arrange
         let mathy = Mathy();
         //Action
-        mathy.multiply(100, 2)
+        mathy.multiply(arg1, arg2)
             .then(res => {
                 //Assert
                 try {
-                    assert.equal(res, 200);
+                    assert.equal(res, arg1*arg2);
                     done();
                 } catch (err) {
-                    done(err)
+                    done(err);
+                }
+            })
+            .catch(done);
+    });
+    it('Should add two numbers', done =>{
+        //Arrange
+        let mathy = Mathy();
+        //Action
+        mathy.add(arg1,arg2)
+            .then(res=>{
+                //Assert
+                try{
+                    assert.equal(res,arg1+arg2);
+                    done();
+                }catch(err){
+                    done(err);
+                }
+            })
+            .catch(done);
+    });
+    it('Should substract one number from another',done=>{
+        //Arrange
+        let mathy = Mathy();
+        //Action
+        mathy.substract(arg1,arg2)
+            .then(res=>{
+                //Assert
+                try{
+                    assert.equal(res,arg1-arg2);
+                    done();
+                }catch(err){
+                    done(err);
+                }
+            })
+            .catch(done);
+    });
+    it('Should divide one number between another',done=>{
+        //Arrange
+        let mathy = Mathy();
+        //Action
+        mathy.divide(arg1,arg2)
+            .then(res=>{
+                try{
+                    assert.equal(res,arg1/arg2);
+                    done();
+                }catch(err){
+                    done(err);
                 }
             })
             .catch(done);
