78c8f7e54901bc7108b00e7f34d6e5355243f31e
Developer <developer@MA-E15-724.local>
2018-01-16 10:45:32 -0600

    Operaciones principales y suma de contenido de un arreglo

--- Mathy/index.js
+++ Mathy/index.js
@@ -2,8 +2,31 @@ module.exports = () => {
     return {
         multiply: (number1, number2) => {
             return new Promise((resolve, reject) => {
-                return resolve(200);
+                return resolve(number1 * number2);
             });
+        },
+
+        add: (number1, number2) => {
+            return new Promise((resolve, reject) => {
+                return resolve(number1 + number2);
+            });
+        },
+
+        substract: (number1, number2) => {
+            return new Promise((resolve, reject) => {
+                return resolve(number1 - number2);
+            });
+        },
+
+        addArray: (array) => {
+            return new Promise((resolve, reject) => {
+                var res = 0;
+                for (i=0; i<array.length; i++){
+                    res += array[i];
+                }
+                return resolve(res);
+            })
+
         }
     }
 };
--- __tests__/Mathy/mathy.expect.js
+++ __tests__/Mathy/mathy.expect.js
@@ -18,4 +18,56 @@ describe('Math example', () => {
             })
             .catch(done);
     });
+
+    it('should add two numbers', done => {
+        //Arrange
+        let mathy = Mathy();
+        //Action
+        mathy.add(100, 2)
+            .then(res => {
+                //Assert
+                try {
+                    assert.equal(res, 102);
+                    done();
+                } catch (err) {
+                    done(err)
+                }
+            })
+            .catch(done);
+    });
+
+    it ('should substract two numbers', done => {
+        //Arrange
+        let mathy = Mathy();
+        //Action
+        mathy.substract(100,50)
+            .then(res=>{
+                //Assert
+                try {
+                    assert.equal(res, 50);
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            })
+            .catch(done);
+    });
+
+    it ('should add numbers of array', done => {
+        //Arrange
+        let mathy = Mathy();
+        let array = [5,5,5,5];
+        //Action
+        mathy.addArray(array)
+            .then(res => {
+                //Assert
+                try{
+                    assert.equal(res, 20);
+                    done();
+                } catch (err){
+                    done(err);
+                }
+            })
+            .catch(done);
+    });
 });
