1d7ab4621dda0d790552005e2946b4ca337a8a5c
JFrankLL <flopez@itexico.net>
2018-01-16 10:29:48 -0600

    basic operators tests

--- __tests__/Mathy/mathy.expect.js
+++ __tests__/Mathy/mathy.expect.js
@@ -2,18 +2,62 @@ const Mathy = require('../../Mathy');
 const assert = require('chai').assert;
 
 describe('Math example', () => {
-    it('should multiply tow numbers', done => {
+    var number1 = 100, number2 = 2;
+    // Multiplication -----------------------------------------------------------------------------
+    it('should multiply two numbers', done => {
         //Arrange
         let mathy = Mathy();
         //Action
-        mathy.multiply(100, 2)
+        mathy.multiply(number1, number2)
             .then(res => {
                 //Assert
                 try {
-                    assert.equal(res, 200);
+                    assert.equal(res, number1 * number2);
                     done();
                 } catch (err) {
-                    done(err)
+                    done(err);
+                }
+            })
+            .catch(done);
+    });
+    // Division -----------------------------------------------------------------------------------
+    it('should divide two numbers', done => {
+        let mathy = Mathy();
+        mathy.divide(number1, number2)
+            .then(res => {
+                try {
+                    assert.equal(res, number1 / number2);
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            })
+            .catch(done);
+    });
+    // Addition -----------------------------------------------------------------------------------
+    it('should add two numbers', done => {
+        let mathy = Mathy();
+        mathy.add(number1, number2)
+            .then(res => {
+                try {
+                    assert.equal(res, number1 + number2);
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            })
+            .catch(done);
+    });
+    // Substraction -------------------------------------------------------------------------------
+    it('should substract two numbers', done => {
+        let mathy = Mathy();
+        mathy.substract(number1, number2)
+            .then(res => {
+                try {
+                    assert.equal(res, number1 - number2);
+                    done();
+                } catch (err) {
+                    done(err);
                 }
             })
             .catch(done);
